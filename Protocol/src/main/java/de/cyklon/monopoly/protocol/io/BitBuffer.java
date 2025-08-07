7976412295ccab5db06e784da04aefe8a2629afb
cyklon73 <cyklon698@gmail.com>
2025-08-07 11:49:11 +0200

    finished java doc for primitive get methodes

--- Protocol/src/main/java/de/cyklon/monopoly/protocol/io/BitBuffer.java
+++ Protocol/src/main/java/de/cyklon/monopoly/protocol/io/BitBuffer.java
@@ -308,28 +308,95 @@ public class BitBuffer {
 		return this;
 	}
 
-	public BitBuffer putVarNumber(long num, int bits, boolean signed) {
-		put(serializeVarNumber(num, bits, signed));
+	/**
+	 * <p> Writes the given amount of bits containing the given numeric value,
+	 * into this buffer at the current position, and then
+	 * increments the position by the set number of bits.  </p>
+	 *
+	 * @param  value
+	 *         The numeric value to be written
+	 *
+	 * @param bits
+	 *         The amount of bits to be used to store the value. allowed range: 1-64 bits
+	 *
+	 * @param signed
+	 *         Marks the value as signed/unsigned. Signed values can be negative and positive. Unsigned values can only be positive
+	 *
+	 * @return  This buffer for chained calls
+	 *
+	 * @throws  BufferOverflowException
+	 *          If there are fewer than sixty-four bits
+	 *          remaining in this buffer
+	 *
+	 * @throws IllegalArgumentException
+	 *          If the given value doesn't fit in the given amount of bits<br>
+	 *          - If the given value is negative but marked as unsigned<br>
+	 *          - If the given bit count is outside the allowed range 1-64
+	 */
+	public BitBuffer putVarNumber(long value, int bits, boolean signed) {
+		put(serializeVarNumber(value, bits, signed));
 		return this;
 	}
 
+	/**
+	 * Reads the bit at this buffer's
+	 * current position, and then increments the position.
+	 *
+	 * @return  The bit at the buffer's current position
+	 *
+	 * @throws  BufferUnderflowException
+	 *          If the buffer's current position is not smaller than its capacity
+	 */
 	public boolean get() {
 		if (pos==bits.length) throw new BufferUnderflowException();
 		return bits[pos++];
 	}
 
-	public synchronized void get(boolean[] dest) {
-		if (pos + dest.length > bits.length) throw new BufferUnderflowException();
-		System.arraycopy(bits, pos, dest, 0, dest.length);
-		pos += dest.length;
+	/**
+	 * <p> This method transfers bits from this buffer into the given
+	 * destination array.
+	 *
+	 * @param   dst
+	 *          The destination array
+	 *
+	 * @throws  BufferUnderflowException
+	 *          If there are fewer than {@code length} bits
+	 *          remaining in this buffer
+	 */
+	public synchronized void get(boolean[] dst) {
+		if (pos + dst.length > bits.length) throw new BufferUnderflowException();
+		System.arraycopy(bits, pos, dst, 0, dst.length);
+		pos += dst.length;
 	}
 
+	/**
+	 * <p> Reads the next eight bits at this buffer's current position,
+	 * composing them into a byte value,
+	 * and then increments the position by eight.  </p>
+	 *
+	 * @return  The byte value at the buffer's current position
+	 *
+	 * @throws  BufferUnderflowException
+	 *          If there are fewer than eight bits
+	 *          remaining in this buffer
+	 */
 	public byte getByte() {
 		boolean[] src = new boolean[Byte.SIZE];
 		get(src);
 		return deserializeByte(src);
 	}
 
+	/**
+	 * <p> Reads the given length*8 bits at this buffer's current position,
+	 * composing them into a byte array with the given length,
+	 * and then increments the position by the length*8.  </p>
+	 *
+	 * @return  The byte array at the buffer's current position with the given length
+	 *
+	 * @throws  BufferUnderflowException
+	 *          If there are fewer than the given length*8 bits
+	 *          remaining in this buffer
+	 */
 	public byte[] getByteArray(int length) {
 		if (pos + length*Byte.SIZE >= bits.length) throw new BufferOverflowException();
 		boolean[] src = new boolean[length*Byte.SIZE];
@@ -337,40 +404,132 @@ public class BitBuffer {
 		return deserializeByteArray(src);
 	}
 
+	/**
+	 * <p> Reads the next sixteen bits at this buffer's current position,
+	 * composing them into a char value,
+	 * and then increments the position by sixteen.  </p>
+	 *
+	 * @return  The char value at the buffer's current position
+	 *
+	 * @throws  BufferUnderflowException
+	 *          If there are fewer than sixteen bits
+	 *          remaining in this buffer
+	 */
 	public char getChar() {
 		boolean[] src = new boolean[Character.SIZE];
 		get(src);
 		return deserializeChar(src);
 	}
 
+	/**
+	 * <p> Reads the next sixteen bits at this buffer's current position,
+	 * composing them into a byte value,
+	 * and then increments the position by sixteen.  </p>
+	 *
+	 * @return  The short value at the buffer's current position
+	 *
+	 * @throws  BufferUnderflowException
+	 *          If there are fewer than sixteen bits
+	 *          remaining in this buffer
+	 */
 	public short getShort() {
 		boolean[] src = new boolean[Short.SIZE];
 		get(src);
 		return deserializeShort(src);
 	}
 
+	/**
+	 * <p> Reads the next thirty-two bits at this buffer's current position,
+	 * composing them into a int value,
+	 * and then increments the position by thirty-two.  </p>
+	 *
+	 * @return  The int value at the buffer's current position
+	 *
+	 * @throws  BufferUnderflowException
+	 *          If there are fewer than thirty-two bits
+	 *          remaining in this buffer
+	 */
 	public int getInt() {
 		boolean[] src = new boolean[Integer.SIZE];
 		get(src);
 		return deserializeInt(src);
 	}
 
+	/**
+	 * <p> Reads the next sixty-four bits at this buffer's current position,
+	 * composing them into a long value,
+	 * and then increments the position by sixty-four.  </p>
+	 *
+	 * @return  The byte value at the buffer's current position
+	 *
+	 * @throws  BufferUnderflowException
+	 *          If there are fewer than eight bits
+	 *          remaining in this buffer
+	 */
 	public long getLong() {
 		boolean[] src = new boolean[Long.SIZE];
 		get(src);
 		return deserializeLong(src);
 	}
 
+	/**
+	 * <p> Reads the next thirty-two bits at this buffer's current position,
+	 * composing them into a float value,
+	 * and then increments the position by thirty-two.  </p>
+	 *
+	 * @return  The float value at the buffer's current position
+	 *
+	 * @throws  BufferUnderflowException
+	 *          If there are fewer than thirty-two bits
+	 *          remaining in this buffer
+	 */
 	public float getFloat() {
 		boolean[] src = new boolean[Float.SIZE];
 		get(src);
 		return deserializeFloat(src);
 	}
 
+	/**
+	 * <p> Reads the sixty-four eight bits at this buffer's current position,
+	 * composing them into a double value,
+	 * and then increments the position by sixty-four.  </p>
+	 *
+	 * @return  The double value at the buffer's current position
+	 *
+	 * @throws  BufferUnderflowException
+	 *          If there are fewer than sixty-four bits
+	 *          remaining in this buffer
+	 */
 	public double getDouble() {
 		boolean[] src = new boolean[Double.SIZE];
 		get(src);
 		return deserializeDouble(src);
+	}
+
+	/**
+	 * <p> Reads the given amount of bits at this buffer's current position,
+	 * composing them into a numeric value,
+	 * and then increments the position by the given bit count.  </p>
+	 *
+	 * @param bits
+	 *         The amount of bits to be used to read the value. allowed range: 1-64 bits
+	 *
+	 * @param signed
+	 *         Marks the value as signed/unsigned. Signed values can be negative and positive. Unsigned values can only be positive
+	 *
+	 * @return  The numeric value at the buffer's current position
+	 *
+	 * @throws  BufferUnderflowException
+	 *          If there are fewer than the given bit count
+	 *          remaining in this buffer
+	 *
+	 * @throws IllegalArgumentException
+	 *          If the given bit count is outside the allowed range 1-64
+	 */
+	public long getVarNumber(int bits, boolean signed) {
+		boolean[] src = new boolean[bits];
+		get(src);
+		return deserializeVarNumber(src, signed);
 	}
 
 
